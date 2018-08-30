b5b5ba761ff25a2e15020ed5835b085bf35e38c3
dacheaux <damjanspace@gmail.com>
2018-08-30 15:35:54 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    const helloWorld = "Zdravo svima!";
+    return (
+      <div className="App">
+        <h2>{helloWorld}</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
